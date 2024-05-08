9d853042e8fc10f2d5880bbe2db0b2a386026744
rjspyk5 <rjspyk5@gmail.com>
2024-05-09 02:28:08 +0600

    find query update for pagination

--- index.js
+++ index.js
@@ -28,7 +28,15 @@ async function run() {
     const productCollection = client.db("emaJohnDB").collection("products");
 
     app.get("/products", async (req, res) => {
-      const result = await productCollection.find().toArray();
+      console.log(req.query);
+      const page = parseInt(req.query.page) - 1 || 0;
+      const size = parseInt(req.query.size) || 10;
+
+      const result = await productCollection
+        .find()
+        .skip(page * size)
+        .limit(size)
+        .toArray();
       res.send(result);
     });
 
